dd5e7ff5bb29d5221dbf2e7d41bc6adfd0806deb
WebNadin <nenadinweb@gmail.com>
2020-06-15 01:40:01 +0300

    sweets timeline first version

--- src/js/partials/ad.js
+++ src/js/partials/ad.js
@@ -1,8 +1,29 @@
 class Ad {
   constructor(item) {
     this.item = item;
+    this.startCup = gsap.timeline();
   }
-  init(){
-    gsap.to(".js-startCup", {duration: 2, delay: .3, y: 60});
+
+  init() {
+    this.startCup.to(".js-startCup", {duration: 2, delay: .3, y: 60});
+    this.startCup.to(".js-sweets-left", {duration: .5, y: -60});
+    this.startCup.to(".js-sweets-right", {duration: .3, y: -30});
+    this.startCup.to(".js-sweets", {duration: 10, delay: .5, y: 1000});
+    //this.startCup.to(".js-sweets-right", {duration: 10, delay: .5, y: 1000});
+    //this.startCup.to(".js-startCup", {duration: 2, delay: .3, y: 60});
+
   }
+
+  startSweets() {
+
+    //sweets.to(".js-sweets", {duration: 5, y: -100, onComplete: this.fallSweets()});
+
+  }
+
+  //fallSweets(){
+  //  let sweets = gsap.timeline();
+  //  sweets.to(".js-sweets-left", {duration: 10, y: 1000});
+  //  sweets.to(".js-sweets-left", {duration: 5, x: 25});
+  //  sweets.to(".js-sweets-left", {duration: 5, x: -25});
+  //}
 }
