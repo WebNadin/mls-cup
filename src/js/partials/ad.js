f6cd699bcacf479f8868d4d8c64ee76bfa1794a7
WebNadin <nenadinweb@gmail.com>
2020-06-15 20:27:21 +0300

    sweets working version

--- src/js/partials/ad.js
+++ src/js/partials/ad.js
@@ -6,24 +6,30 @@ class Ad {
   init() {
     gsap.to(".js-startCup", {duration: .9, delay: .3, y: 60});
     gsap.to(".js-startCup", {duration: .6, delay: .3, onComplete: this.startSweets});
+    gsap.to(".js-startCup", {duration: .6, delay: 3.3, onComplete: this.startPeople});
   }
 
   startSweets() {
     gsap.to(".js-sweets-left", {duration: 1, y: 0, x: 50});
     gsap.to(".js-sweets-right", {duration: 1, y: -50, x: -50});
     gsap.to(".js-sweets-bottom", {duration: 1, y: 300, x: 0});
-    gsap.to(".js-sweets", {duration: 30, delay: 1.3, y: 1000});
-    gsap.to(".js-sweets-left", {duration: 3, delay: 1.3, x: -50});
-    gsap.to(".js-sweets-right", {duration: 3, delay: 1.3, x: 50});
-    gsap.to(".js-sweets-left", {duration: 3, delay: 4.3, x: 50});
-    gsap.to(".js-sweets-right", {duration: 3, delay: 4.3, x: -50});
-    gsap.to(".js-sweets-left", {duration: 3, delay: 7.3, x: -50});
-    gsap.to(".js-sweets-right", {duration: 3, delay: 7.3, x: 50});
-    gsap.to(".js-sweets-left", {duration: 3, delay: 10.3, x: 50});
-    gsap.to(".js-sweets-right", {duration: 3, delay: 10.3, x: -50});
-    gsap.to(".js-sweets-left", {duration: 3, delay: 13.3, x: -50});
-    gsap.to(".js-sweets-right", {duration: 3, delay: 13.3, x: 50});
-    gsap.to(".js-sweets-left", {duration: 3, delay: 16.3, x: 50});
-    gsap.to(".js-sweets-right", {duration: 3, delay: 16.3, x: -50});
+    gsap.to(".js-sweets", {duration: 30, delay: 1, y: 1000});
+    gsap.to(".js-sweets-left", {duration: 3, delay: 1, x: -50});
+    gsap.to(".js-sweets-right", {duration: 3, delay: 1, x: 50});
+    gsap.to(".js-sweets-left", {duration: 3, delay: 4, x: 50});
+    gsap.to(".js-sweets-right", {duration: 3, delay: 4, x: -50});
+    gsap.to(".js-sweets-left", {duration: 3, delay: 7, x: -50});
+    gsap.to(".js-sweets-right", {duration: 3, delay: 7, x: 50});
+    gsap.to(".js-sweets-left", {duration: 3, delay: 10, x: 50});
+    gsap.to(".js-sweets-right", {duration: 3, delay: 10, x: -50});
+    gsap.to(".js-sweets-left", {duration: 3, delay: 13, x: -50});
+    gsap.to(".js-sweets-right", {duration: 3, delay: 13, x: 50});
+    gsap.to(".js-sweets-left", {duration: 3, delay: 16, x: 50});
+    gsap.to(".js-sweets-right", {duration: 3, delay: 16, x: -50});
+  }
+
+
+  startPeople(){
+
   }
 }
