0114ecff2d1ea3a8259250d978a6be07ebe2005e
WebNadin <nenadinweb@gmail.com>
2020-06-16 00:23:28 +0300

    people textOne animation was added

--- src/js/partials/ad.js
+++ src/js/partials/ad.js
@@ -7,25 +7,16 @@ class Ad {
     gsap.to(".js-startCup", {duration: .9, delay: .3, y: 60});
     gsap.to(".js-startCup", {duration: .6, delay: .3, onComplete: this.startSweets});
     gsap.to(".js-startCup", {duration: .6, delay: 3.3, onComplete: this.startPeople});
+    gsap.to(".js-startCup", {duration: .6, delay: 5.3, onComplete: this.startTextGlory});
   }
 
   startSweets() {
     gsap.to(".js-sweets-left", {duration: 1, y: 0, x: 50});
     gsap.to(".js-sweets-right", {duration: 1, y: -50, x: -50});
     gsap.to(".js-sweets-bottom", {duration: 1, y: 300, x: 0});
-    gsap.to(".js-sweets", {duration: 30, delay: 1, y: 1000});
+    gsap.to(".js-sweets", {duration: 7, delay: 1, y: 500});
     gsap.to(".js-sweets-left", {duration: 3, delay: 1, x: -50});
-    gsap.to(".js-sweets-right", {duration: 3, delay: 1, x: 50});
     gsap.to(".js-sweets-left", {duration: 3, delay: 4, x: 50});
-    gsap.to(".js-sweets-right", {duration: 3, delay: 4, x: -50});
-    gsap.to(".js-sweets-left", {duration: 3, delay: 7, x: -50});
-    gsap.to(".js-sweets-right", {duration: 3, delay: 7, x: 50});
-    gsap.to(".js-sweets-left", {duration: 3, delay: 10, x: 50});
-    gsap.to(".js-sweets-right", {duration: 3, delay: 10, x: -50});
-    gsap.to(".js-sweets-left", {duration: 3, delay: 13, x: -50});
-    gsap.to(".js-sweets-right", {duration: 3, delay: 13, x: 50});
-    gsap.to(".js-sweets-left", {duration: 3, delay: 16, x: 50});
-    gsap.to(".js-sweets-right", {duration: 3, delay: 16, x: -50});
   }
 
 
@@ -35,5 +26,9 @@ class Ad {
     gsap.to(".js-peopleLeft", {duration: .3, opacity: 1});
     gsap.to(".js-peopleRight", {duration: 2, ease: Power1.easeOut, x: 0});
     gsap.to(".js-peopleRight", {duration: .3, opacity: 1});
+  }
+
+  startTextGlory() {
+    gsap.to(".js-one", {duration: .7, opacity: 1, scale: 1, ease: Expo.easeOut})
   }
 }
