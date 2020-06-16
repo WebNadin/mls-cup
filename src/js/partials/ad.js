a853eaf3db557647f9ffa03b0256daaa2b7901f3
WebNadin <nenadinweb@gmail.com>
2020-06-16 04:09:15 +0300

    people block animation was finished

--- src/js/partials/ad.js
+++ src/js/partials/ad.js
@@ -8,6 +8,7 @@ class Ad {
     gsap.to(".js-startCup", {duration: .6, delay: .3, onComplete: this.startSweets});
     gsap.to(".js-startCup", {duration: .6, delay: 3.3, onComplete: this.startPeople});
     gsap.to(".js-startCup", {duration: .6, delay: 5.3, onComplete: this.startTextGlory});
+    gsap.to(".js-startCup", {duration: .6, delay: 11, onComplete: this.finishPeople});
   }
 
   startSweets() {
@@ -19,7 +20,6 @@ class Ad {
     gsap.to(".js-sweets-left", {duration: 3, delay: 4, x: 50});
   }
 
-
   startPeople() {
     gsap.to(".js-blackening", {duration: 2, delay: .3, ease: Power1.easeOut, opacity: 1});
     gsap.to(".js-peopleLeft", {duration: 2, ease: Power1.easeOut, x: 0});
@@ -29,6 +29,37 @@ class Ad {
   }
 
   startTextGlory() {
-    gsap.to(".js-one", {duration: .7, opacity: 1, scale: 1, ease: Expo.easeOut})
+    gsap.to(".js-one", {duration: .7, opacity: 1, scale: 1, ease: Expo.easeOut});
+    gsap.to(".js-game", {duration: .5, delay: 1, opacity: 1, ease: Back.easeOut.config(1.1), y: 0});
+
+    gsap.to(".js-one", {duration: .2, delay: 1.2, y: -30, ease: Back.easeOut.config(1.1)});
+    gsap.to(".js-one", {duration: 1, delay: 1.4, y: 0});
+    gsap.to(".js-one", {duration: .2, delay: 1.4, rotate: 5});
+    gsap.to(".js-one", {duration: .2, delay: 1.6, rotate: -5});
+    gsap.to(".js-one", {duration: .2, delay: 1.8, rotate: 3});
+    gsap.to(".js-one", {duration: .2, delay: 2, rotate: -3});
+    gsap.to(".js-one", {duration: .2, delay: 2.2, rotate: 0});
+    gsap.to(".js-one", {duration: .2, delay: 2.4, rotate: 0});
+
+    gsap.to(".js-glory", {duration: .6, delay: 2.2, opacity: 1, scale: 1});
+
+    gsap.to(".js-logos", {duration: .1, delay: 2.2, opacity: .3});
+    gsap.to(".js-logos", {duration: 1.5, delay: 2.2, opacity: 1, scale: 1, ease: Expo.easeOut});
+
+    gsap.to(".js-glory-copyLeft", {duration: .3, delay: 3.2, opacity: .1, scale: 1.4});
+    gsap.to(".js-glory-copyRight", {duration: .3, delay: 3.2, opacity: .1, scale: 1.4});
+    gsap.to(".js-glory-copyLeft", {duration: 3, delay: 3.3, opacity: 0});
+    gsap.to(".js-glory-copyRight", {duration: 3, delay: 3.3, opacity: 0});
+    gsap.to(".js-glory-copyLeft", {duration: 3, delay: 3.3, x: 200, y: 30});
+    gsap.to(".js-glory-copyRight", {duration: 3, delay: 3.3, x: -200, y: 30});
+    gsap.to(".js-textGlory", {duration: 3, delay: 3.6, opacity: 0});
+    gsap.to(".js-blackening", {duration: .1, delay: 1, opacity: 0});
+  }
+
+  finishPeople() {
+    gsap.to(".js-peopleLeft", {duration: 8, ease: Power1.easeOut, x: -2000});
+    gsap.to(".js-logosLeft", {duration: 8, ease: Power1.easeOut, x: -2000});
+    gsap.to(".js-peopleRight", {duration: 8, ease: Power1.easeOut, x: 2000});
+    gsap.to(".js-logosRight", {duration: 8, ease: Power1.easeOut, x: 2000});
   }
 }
